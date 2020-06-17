023868e880aa9636a0cbf45cbbf69d2eae86f23c
Bryce Whittington <brycew30@gmail.com>
2020-06-17 22:52:55 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+katz_deli = []
+def line(array)
+ line_array = []
+ if array.length == 0
+   puts "The line is currently empty."
+ else
+   array.each.with_index(1) do |name, index|
+    line_array.push("#{index}. #{name}")
+   end
+   puts "The line is currently: #{line_array.join(" ")}"
+ end
+end
